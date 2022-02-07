490a243f3bcaa39a17c954e7605202ba05490709
Sikander Sandhu <sikandersandhu82@gmail.com>
2022-02-07 14:37:07 +1100

    Add new pizza tile modeling

---
+++ WebTestAssessment/UI/PizzaTile.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace WebTestsAssessment
+{
+    public class PizzaTile
+    {
+        private IWebElement pizza;
+
+        public PizzaTile(IWebElement pizza)
+        {
+            this.pizza = pizza;
+        }
+
+        public string Pizza => pizza.FindElement(By.ClassName("name")).Text;
+
+        public double DoublePrice
+        {
+            get
+            {
+                string unformatted = pizza.FindElement(By.ClassName("price")).Text;
+                string formatted = unformatted.Replace("$", "");
+                return Convert.ToDouble(formatted);
+            }
+        }
+    }
+}
