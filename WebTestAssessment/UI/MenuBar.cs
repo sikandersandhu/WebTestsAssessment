9cf61d33b3432ae74a8305dc9906a49ddc06384f
Sikander Sandhu <sikandersandhu82@gmail.com>
2022-02-07 14:35:52 +1100

    Add new menu bar modeloin

---
+++ WebTestAssessment/UI/MenuBar.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace WebTestsAssessment
+{
+    public class MenuBar
+    {
+        private ChromeDriver driver;
+
+        public MenuBar(ChromeDriver driver)
+        {
+            this.driver = driver;
+        }
+        public void OpenLoginOrSignupPage()
+        {
+            driver.FindElement(By.CssSelector("[aria-label='login or signup']")).Click();
+        }
+        public void OpenMenuPage()
+        {
+            driver.FindElement(By.CssSelector("[aria-label='menu']")).Click();
+        }
+
+    }
+}
